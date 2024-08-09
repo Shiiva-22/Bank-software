6d4053afb46f6699fd906db6cb4cd1e3d4143e74
shiiva-22 <skweb086@gmail.com>
2024-08-09 20:12:27 +0530

    Bank software with three pages

--- Desktop/BANK-SOFTWARE/src/CreateAccount.jsx
+++ Desktop/BANK-SOFTWARE/src/CreateAccount.jsx
@@ -1,24 +1,67 @@
-
+import { useState } from "react";
+import "./CreateAccount.css";
 
 const CreateAccount=()=>{
+
+
+    const [username,setname]= useState('');
+    const [userAge,setAge]= useState('');
+    const [userAccountNo,setAccountNo] = useState('');
+    const [userAccountType,setAccountType] = useState("");
+    const [userAmount,setAmount] = useState("");
+
+
+    const Submit=()=>{
+
+        let newuserdata ={
+        AccountNo:userAccountNo,
+          Name:username,
+          Age:userAge,
+          AccountType:userAccountType,
+          Balance:userAmount
+        }
+     data.push(newuserdata);
+     console.log(newuserdata);
+        
+    }
+
     return(
         <div>
-            <form>
+            <form className="Form-wrapper" >
                 <input type="text"
                 placeholder="Enter Name"
+                onChange={(e)=>setname(e.target.value)}
+                /><br/>
+                <input type="number"
+                placeholder="Enter Age"
+                onChange={(e)=>setAge(e.target.value)}
                 
                 /><br/>
                 <input
                 type="number"
+                placeholder="Enter Account Number"
+                onChange={(e)=>setAccountNo(e.target.value)}
+
                 /><br/>
                 <select>
-                <option type="disabled" > Account Type</option>
+               
+                <option disabled  onChange={(e)=>setAccountType(e.target.value)}> Account Type</option>
 
                     <option value="Salary Account">Salary Account</option>
                     <option value="Current Account">Current Account</option>
                     <option value="Saving Account">Saving Account</option>
 
-                </select>
+                </select><br/>
+
+                <input type="number"
+                placeholder="enter Amount"
+                onChange={(e)=>setAmount(e.target.value)}
+
+                /><br/>
+
+                <button onClick={Submit}>Submit</button>
+                
+                
 
                 
                 
